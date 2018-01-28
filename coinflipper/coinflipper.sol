37c4bd498a65031d19ae89d43a1e765796302408
shonjs <rush2shon@gmail.com>
2018-01-28 14:39:06 +0530

    common modifier added for game state

--- coinflipper/coinflipper.sol
+++ coinflipper/coinflipper.sol
@@ -1,38 +1,44 @@
-pragma solidity ^0.4.19;
-contract coinFlipper{
-	mapping(uint=>address) parties;
-	uint bet;
-	uint[] private variables = new uint[](2);
+	pragma solidity ^0.4.19;
+	contract coinFlipper{
+		mapping(uint=>address) parties;
+		uint bet;
+		uint[] private variables = new uint[](2);
 
-	enum GameState { betOpen, betWaiting, betClosed}
-	GameState public coinFlip;
+		enum GameState { betOpen, betWaiting, betClosed}
+		GameState public coinFlip;
 
-	function coinFlipper() public{
-		coinFlip = GameState.betOpen;
-	}
-
-	function offerBet(uint _rand) public payable {
-		require(coinFlip == GameState.betOpen);
-		bet = msg.value;
-		coinFlip = GameState.betWaiting;
-		parties[0] = msg.sender;
-		variables[0] = _rand;
-	}
-
-	function meetBet(uint _rand) public payable {
-		require(coinFlip == GameState.betWaiting);
-		require(msg.value >= bet);
-		coinFlip = GameState.betClosed;
-		parties[1] = msg.sender;
-		variables[1] = _rand;
-	}
-
-	function flipCoin() public{
-		if(((variables[0] * block.number) + (variables[1] * block.timestamp))% 2 == 0){
-			parties[0].send(this.balance);
-		} else {
-			parties[1].send(this.balance);
+		function coinFlipper() public{
+			coinFlip = GameState.betOpen;
 		}
-		coinFlip = GameState.betOpen;
+
+		modifier gameOn(GameState _state){
+			require(coinFlip == _state);
+			_;
+		}
+
+		function offerBet(uint _rand) public payable 
+		gameOn(GameState.betOpen) {		
+			bet = msg.value;
+			coinFlip = GameState.betWaiting;
+			parties[0] = msg.sender;
+			variables[0] = _rand;
+		}
+
+		function meetBet(uint _rand) public payable 
+		gameOn(GameState.betWaiting){
+			require(msg.value >= bet);
+			coinFlip = GameState.betClosed;
+			parties[1] = msg.sender;
+			variables[1] = _rand;
+		}
+
+		function flipCoin() public 
+		gameOn(GameState.betClosed){
+			if(((variables[0] * block.number) + (variables[1] * block.timestamp))% 2 == 0){
+				parties[0].send(this.balance);
+			} else {
+				parties[1].send(this.balance);
+			}
+			coinFlip = GameState.betOpen;
+		}
 	}
-}
