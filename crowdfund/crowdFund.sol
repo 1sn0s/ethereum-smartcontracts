f004d1ed5ac460e763c1bdca74ee599d82d553eb
shonjs <rush2shon@gmail.com>
2018-01-30 01:18:43 +0530

    crowdfund contract change

--- crowdfund/crowdFund.sol
+++ crowdfund/crowdFund.sol
@@ -1,23 +1,33 @@
 pragma solidity ^0.4.19;
 
 contract crowdFund{
+	address public beneficiary;
+	uint public goal;
+	uint public deadline;
+	uint public fundAchieved;
 	mapping(address=>uint) contribution;
-	uint goal;
-	uint public current;
 
 	enum FundStatus {stopped, inProgress, goalAchieved, goalNotAchieved, refundInProgress, Completed}
 	FundStatus _fundStatus;
 
-	function crowdFund(){
+	function crowdFund(address _beneficiary, uint _goal, uint _deadline){
+		beneficiary = _beneficiary;
+		goal 		= _goal;
+		deadline 	= _deadline;
 		_fundStatus = FundStatus.inProgress;
 	}
 
-	modifier _fundStatus(FundStatus _status){
+	modifier _FundStatus(FundStatus _status){
 		require(_status == _fundStatus);
 		_;
 	}
 
-	function contribute() public _fundStatus(FundStatus.inProgress){
+	modifier _AcceptFund(FundStatus _status, uint _now){
+		require(_status == FundStatus.inProgress && _now <= deadline);
+		_;
+	}
+
+	function contribute() public _AcceptFund(FundStatus.inProgress, now){
 		//TODO: Check if value exceeds. Then refund rest.
 		contribution[msg.sender] = msg.value;
 	}
