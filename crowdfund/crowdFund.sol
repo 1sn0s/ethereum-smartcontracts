e52df56df9ce169e6c17c3dff9f25b685fe9ce20
shonjs <rush2shon@gmail.com>
2018-01-31 00:31:49 +0530

    contract changes

--- crowdfund/crowdFund.sol
+++ crowdfund/crowdFund.sol
@@ -2,6 +2,7 @@ pragma solidity ^0.4.19;
 
 contract crowdFund{
 	address public beneficiary;
+	address private _owner;
 	uint public goal;
 	uint public deadline;
 	uint public fundAchieved;
@@ -15,20 +16,55 @@ contract crowdFund{
 		goal 		= _goal;
 		deadline 	= _deadline;
 		_fundStatus = FundStatus.inProgress;
+		_owner		= msg.sender;
 	}
 
-	modifier _FundStatus(FundStatus _status){
+	modifier fundStatus(FundStatus _status){
 		require(_status == _fundStatus);
 		_;
 	}
 
-	modifier _AcceptFund(FundStatus _status, uint _now){
+	modifier acceptFund(FundStatus _status, uint _now){
 		require(_status == FundStatus.inProgress && _now <= deadline);
 		_;
 	}
 
-	function contribute() public _AcceptFund(FundStatus.inProgress, now){
-		//TODO: Check if value exceeds. Then refund rest.
-		contribution[msg.sender] = msg.value;
+	modifier isOwner(_address){
+		require(_owner == _address);
+		_;
 	}
+
+	function contribute() public payable acceptFund(FundStatus.inProgress, now){
+		//TODO: Check if value exceeds. Then refund rest.
+		contribution[msg.sender] += msg.value;
+		fundAchieved += msg.value;
+		if(fundAchieved >= goal){
+			_fundStatus = FundStatus.goalAchieved;
+		}
+	}
+
+	//Close the fund.
+	function closeFund() public returns(uint fundStatus){
+		if(_isOwner(msg.sender)){
+			_fundStatus = FundStatus.stopped;
+		}
+		if(fundAchieved >= goal){
+			_fundStatus = FundStatus.goalAchieved;
+		} else {
+			_fundStatus = FundStatus.goalNotAchieved;
+		}
+		returns _fundStatus;
+	}
+
+	function _isOwner(address _address) private returns(bool isOwner){
+		return (_address == _owner);
+	}
+
+	//Checking if the dealine has passed.
+	//TODO: Check if this condition is safe/secure
+	function _isPastDeadLine() private returns(bool isPastDealine){
+		return now >= deadline;
+	}
+
+
 }
