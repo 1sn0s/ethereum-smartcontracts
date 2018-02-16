ccf2d6d805c10c31cb591794ecad2486f4e25878
shonjs <rush2shon@gmail.com>
2018-02-17 05:00:50 +0530

    Multi sig contract execution

--- multisigwallet/MultiSigContract.sol
+++ multisigwallet/MultiSigContract.sol
@@ -11,18 +11,34 @@ contract MultiSigContract{
 		require(ownersToSet.length <= sigCount && sigCount != 0);
 		//The addresses should be sent in an increasing order
 		//This is to avoid the duplicate check
-		address _lastAddress = address(0);
+		address lastAddress = address(0);
 		for(uint ownerIndex= 0; ownerIndex < ownersToSet.length; ownerIndex++){
 			require(ownersToSet[ownerIndex] > _lastAddress);			
 			_isOwner[ownersToSet[ownerIndex]] = true;
-			_lastAddress = ownersToSet[ownerIndex];
+			lastAddress = ownersToSet[ownerIndex];
 		}
 		ownersList = ownersToSet;
 		sigLimit = sigCount;
 	}
 
-	function execute(string sigv, string sigR, string sigS, 
+	function execute(string[] sigV, string[] sigR, string[] sigS, 
 		address destination, uint value, string data){
-		//EIP 191
+		//ERC 191
+		require(sigv.length == sigLimit);
+		require(sigR.lenght == sigS.length && sigR.length == sigV.length);
+
+		byte32 txHash = keccak256(byte(0x19), byte(0), this, destination, value, data, nonce);
+
+		address lastAddress = address(0);
+		for(uint index; index<sigLimit; index++){
+			address signer = ecrecover(txHash, sigV[index], sigR[index], sigS[index]);
+			require(signer > lastAddress && _isOwner[signer]);
+			lastAddress = signer;
+		}
+
+		nonce = nonce + 1;
+		require(destination.call.value(value)(data));
 	}
+
+	function () payable {}
 }
